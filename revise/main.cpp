7367c4c9faf0fd1dc9486041abc7ef23060e4346
Rahul1629 <44530083+Rahul1629@users.noreply.github.com>
2018-10-27 22:59:18 +0530

    Update main.cpp

--- revise/main.cpp
+++ revise/main.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
+#9include<string.h>
 
 using namespace std;
 
 int main()
 {
-    string a,b;
+    /*string a,b;
     string c;
     cin>>a>>b;
     c=a+b;
+    cout<<c;*/
+    //you should do it in another way for any less no of characters.
+    char a[100],b[100],c[200];
+    cin>>a>>b;//input strings a and b as character arrays.
+    c=strcat(a,b);//concatenate these strings
     cout<<c;
+    
+    
+    
     return 0;
 }
